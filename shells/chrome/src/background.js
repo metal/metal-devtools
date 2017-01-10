a58b486e73f946efb7b8f9c375d7cbcdc679fe7f
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2017-01-10 08:23:59 -0800

    Check tabId before posting message

--- shells/chrome/src/background.js
+++ shells/chrome/src/background.js
@@ -1,7 +1,9 @@
 import './popup.html';
 
 chrome.extension.onConnect.addListener(function(port) {
-	chrome.extension.onMessage.addListener(function (message) {
+	chrome.extension.onMessage.addListener(function (message, sender) {
+		if (port.name === sender.tab.id.toString()) {
 			port.postMessage(message);
+		}
 	});
 });
--- shells/chrome/src/panel.js
+++ shells/chrome/src/panel.js
@@ -6,6 +6,6 @@ import App from '../../../frontend/components/App';
 new App(
 	{
 		element: document.getElementById('container'),
-		port: chrome.extension.connect({name: 'metal-devtools'})
+		port: chrome.extension.connect({name: chrome.devtools.inspectedWindow.tabId.toString()})
 	}
 );
