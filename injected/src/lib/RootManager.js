11df3b6d267b75d7709853cd052c7d888b33e6da
Michael Hadley <michael.hadley@liferay.com>
2017-01-31 10:48:17 -0800

    Use helper

--- injected/src/lib/RootManager.js
+++ injected/src/lib/RootManager.js
@@ -4,8 +4,26 @@ import processDataManagers from './processDataManagers';
 
 const __METAL_DEV_TOOLS_COMPONENT_KEY__ = '__METAL_DEV_TOOLS_COMPONENT_KEY__';
 
+const maskDimensionStyles = {
+	background: 'rgba(0, 0, 0, 0.65)',
+	color: '#FFFFFF',
+	'font-size': '11px',
+	padding: '2px'
+};
+
+const maskStyles = {
+	'align-items': 'flex-end',
+	background: 'rgba(111, 168, 220, 0.65)',
+	position: 'fixed',
+	'z-index': 10000
+};
+
 let componentId = 0;
 let updateScheduled = {};
+
+function applyStyles(element, styles) {
+	Object.keys(styles).forEach(name => element.style[name] = styles[name]);
+}
 
 class RootManager extends EventEmitter {
 	constructor() {
@@ -13,8 +31,8 @@ class RootManager extends EventEmitter {
 
 		this._componentMap = {};
 		this._listeners = {};
-		this._mask;
-		this._maskDimensions;
+		this._mask = null;
+		this._maskDimensions = null;
 		this._messenger = new Messenger();
 		this._roots = [];
 
@@ -29,26 +47,16 @@ class RootManager extends EventEmitter {
 	}
 
 	createOverlayMask() {
-		const div = document.createElement('div');
+		this._mask = document.createElement('div');
 		this._maskDimensions = document.createElement('span');
 
-		this._maskDimensions.style.background = 'rgba(0, 0, 0, 0.65)';
-		this._maskDimensions.style.color = '#FFFFFF';
-		this._maskDimensions.style.padding = '2px';
-		this._maskDimensions.style['font-size'] = '11px';
+		applyStyles(this._maskDimensions, maskDimensionStyles);
+		applyStyles(this._mask, maskStyles);
 
-		div.append(this._maskDimensions);
+		this._mask.setAttribute('id', '__METAL_DEV_TOOLS_MASK__');
 
-		div.setAttribute('id', '__METAL_DEV_TOOLS_MASK__');
-
-		div.style.background = 'rgba(111, 168, 220, 0.65)';
-		div.style.position = 'fixed';
-		div.style['z-index'] = 10000;
-		div.style['align-items'] = 'flex-end';
-
-		this._mask = div;
-
-		document.body.append(div);
+		this._mask.append(this._maskDimensions);
+		document.body.append(this._mask);
 	}
 
 	getComponentNode(id) {
@@ -76,11 +84,17 @@ class RootManager extends EventEmitter {
 			const {height, left, top, width} = element.getBoundingClientRect();
 
 			this._maskDimensions.innerHTML = `${width}px x ${height}px`;
-			this._mask.style.display = 'flex';
-			this._mask.style.height = height ? `${height}px` : 'initial';
-			this._mask.style.left = `${left}px`;
-			this._mask.style.top = `${top}px`;
-			this._mask.style.width = width ? `${width}px` : 'initial';
+
+			applyStyles(
+				this._mask,
+				{
+					display: 'flex',
+					height: height ? `${height}px` : 'initial',
+					left: `${left}px`,
+					top: `${top}px`,
+					width: width ? `${width}px` : 'initial'
+				}
+			);
 		}
 		else {
 			this._mask.style.display = 'none';
